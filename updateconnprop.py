0e990025ed2a149e63ed767643aed2179e1472c2
Victor <33645782+mrvictoru@users.noreply.github.com>
2021-08-31 16:09:53 +0800

    init test.py

--- updateconnprop.py
+++ updateconnprop.py
@@ -1,1 +1,23 @@
 import arcpy
+
+# get script parameter
+p = arcpy.mp.ArcGISProject('current')
+m = p.activeMap
+l = m.listLayers()
+newconnprop = arcpy.GetParameterasText(0)
+
+# indicates current map name
+pmsg = "Current map:" + m.name
+arcpy.AddMessage(pmsg)
+
+# loop through the layers in the map
+for layer in l:
+    arcpy.AddMessage(layer.name)
+    try:
+        print(layer.connectionProperties)
+        layer.updateConnectionProperties(layer.connectionProperties, newconnprop)
+    except:
+        print
+
+
+
