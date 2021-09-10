c0a40ce475a76535e71df1f4740bf450d96d69fd
Victor <33645782+mrvictoru@users.noreply.github.com>
2021-09-10 17:33:11 +0800

    add comment and put section of code in functions

--- backupmap.py
+++ backupmap.py
@@ -1,63 +1,83 @@
-import arcpy, sys, os
+import arcpy, sys, os, json
+
+# call export asset package API
+def UN2AP(utility_network, domain_networks, output_path, output_name, export_data):
+    try:
+        arcpy.pt.UtilityNetworkToAssetPackage(utility_network, 
+                                            domain_networks, 
+                                            output_path, 
+                                            output_name, 
+                                            export_data)
+        arcpy.AddMessage("Export successed")
+
+    except Exception as e:
+        pmsg = "Unable to export UN to asset package, error: " + str(e)
+        arcpy.AddError(pmsg)
+        sys.exit(1)
+
+def updateconnpropgdb(target_path, mapping_path, l):
+    #check workspace:
+    if "gdb" in target_path:
+        workspace_factory = "File Geodatabase"
+        conn_base = "database"
+    else:
+        arcpy.AddError("target is not Geodatabase")
+        sys.exit(0)
+
+    # get dataset mapping json list
+    jsonlist = []
+    jlist = os.listdir(mapping_path)
+    for jfile in jlist:
+        jpath = os.path.join(mapping_path,jfile)
+        with open(jpath) as f:
+            data = json.load(f)
+            jsonlist.append(data)
+
+    # loop through the layers in the map
+    for layer in l:
+        if layer.supports("CONNECTIONPROPERTIES"):
+            new_conn = layer.connectionProperties
+            # set new connection info to database
+            new_conn['connection_info'] = {}
+            new_conn['connection_info']["database"] = target_path
+            # disregard lower case when check for dataset mapping
+            for jmapping in jsonlist:
+                if layer.name.lower() in (string.lower() for string in jmapping["feature"]):
+                    pmsg = "dataset: " + str(new_conn["dataset"]) + " to " + str(jmapping["dataset"])
+                    arcpy.AddMessage(pmsg)
+                    new_conn["dataset"] = jmapping["dataset"]
+                    break
+            # set to appropriate workspace factory
+            new_conn["workspace_factory"] = workspace_factory
+            arcpy.AddMessage(layer.name)
+            pmsg = str(layer.connectionProperties) + " updating"
+            arcpy.AddMessage(pmsg)
+            pmsg = "to " + str(new_conn) + " new connection"
+            arcpy.AddMessage(pmsg)
+            layer.updateConnectionProperties(layer.connectionProperties, new_conn,True,False,False)
+            pmsg = str(layer.connectionProperties) + "updated"
+            arcpy.AddMessage(pmsg)
 
 # set script parameter
 p = arcpy.mp.ArcGISProject('current') # set project to current project
 m = p.activeMap # get current map name
+layers = m.listLayers() # get layers in map
 domain_networks = ["*"] #select all
 export_data = "INCLUDE_DATA" #to load data from all working from all feature classes
-output_name = arcpy.GetParameter(2)
+output_name = arcpy.GetParameter(2) # get output file name
+utility_network = arcpy.GetParameter(0) # get UN for creating asset package
+output_path = arcpy.GetParameter(1) # get Destination path
+dataset_map_path = arcpy.GetParameter(3) # get Dataset mapping json(s) folder path 
 
-# indicate Current Project and Map
-pmsg = "Current Project: " + str(p.filePath) + " Current Map: " + str(m_name)
-arcpy.AddMessage(pmsg)
-
-# get UN for creating asset package
-utility_network = arcpy.GetParameter(0)
-pmsg = "Utility Network: " + str(utility_network)
-arcpy.AddMessage(pmsg)
-
-# get Destination path
-output_path = arcpy.GetParameter(1)
-pmsg = "Output: " + str(output_path)
-arcpy.AddMessage(pmsg)
 
 #export assetpackage
 arcpy.AddMessage("Starting to create asset package...")
-
-try:
-    arcpy.pt.UtilityNetworkToAssetPackage(utility_network, 
-                                          domain_networks, 
-                                          output_path, 
-                                          output_name, 
-                                          export_data)
-    arcpy.AddMessage("Export successed")
-
-except Exception as e:
-    pmsg = "Unable to export UN to asset package, error: " + str(e)
-    arcpy.AddError(pmsg)
-    sys.exit(1)
+UN2AP(utility_network=utility_network, domain_networks=domain_networks, output_path=output_path, output_name=output_name, export_data=export_data)
 
 # update connection properties
-pmsg = "Updating connection properties in " + str(m.name) + "..."
-arcpy.AddMesage(pmsg)
-
-new = os.path.join(str(output_path),str(output_name) + ".gdb")
-l = m.listLayers()
-
-# loop through the layers in the map
-for layer in l:
-    try:
-        arcpy.AddMessage(layer.name)
-        pmsg = str(layer.connectionProperties) + "updating"
-        new_conn = layer.connectionProperties
-        new_conn['connection_info'] = new
-        arcpy.AddMessage(pmsg)
-        layer.updateConnectionProperties(layer.connectionProperties, new_conn)
-        pmsg = str(layer.connectionProperties + "updated")
-        arcpy.AddMessage(pmsg)
-    except:
-        pmsg = str(layer.name) + "Layer Conn Prop Null"
-        arcpy.AddMessage(pmsg)
+arcpy.AddMesage("Updating connection properties")
+new = os.path.join(str(output_path),str(output_name) + ".gdb") # set New connection target to newly create asset package
+updateconnpropgdb(target_path=new, mapping_path=dataset_map_path, l = layers) 
 
 
 # create new backup project at output path
--- updateconnprop.py
+++ updateconnprop.py
@@ -1,9 +1,13 @@
-import arcpy, json, os
+import arcpy, json, os, sys
 
-def updateconnprop(target_path, mapping_path):
+def updateconnpropgdb(target_path, mapping_path, l):
     #check workspace:
     if "gdb" in target_path:
         workspace_factory = "File Geodatabase"
+        conn_base = "database"
+    else:
+        arcpy.AddError("target is not Geodatabase")
+        sys.exit(0)
 
     # get dataset mapping json list
     jsonlist = []
@@ -14,39 +18,38 @@ def updateconnprop(target_path, mapping_path):
             data = json.load(f)
             jsonlist.append(data)
 
-    # indicates current map name
-    pmsg = "Current map:" + m.name + "\n"
-    arcpy.AddMessage(pmsg)
-
     # loop through the layers in the map
     for layer in l:
-        try:
-            arcpy.AddMessage(layer.name)
+        if layer.supports("CONNECTIONPROPERTIES"):
             new_conn = layer.connectionProperties
-            new_conn['connection_info'] = target_path
+            # set new connection info to database
+            new_conn['connection_info'] = {}
+            new_conn['connection_info']["database"] = target_path
+            # disregard lower case when check for dataset mapping
             for jmapping in jsonlist:
-                if layer.name in jmapping["feature"]:
+                if layer.name.lower() in (string.lower() for string in jmapping["feature"]):
+                    pmsg = "dataset: " + str(new_conn["dataset"]) + " to " + str(jmapping["dataset"])
+                    arcpy.AddMessage(pmsg)
                     new_conn["dataset"] = jmapping["dataset"]
                     break
+            # set to appropriate workspace factory
             new_conn["workspace_factory"] = workspace_factory
+            arcpy.AddMessage(layer.name)
             pmsg = str(layer.connectionProperties) + " updating"
             arcpy.AddMessage(pmsg)
             pmsg = "to " + str(new_conn) + " new connection"
             arcpy.AddMessage(pmsg)
-            layer.updateConnectionProperties(layer.connectionProperties, new_conn)
-            pmsg = str(layer.connectionProperties + "updated")
-            arcpy.AddMessage(pmsg)
-        except:
-            pmsg = str(layer.name) + "Layer Conn Prop Null"
+            layer.updateConnectionProperties(layer.connectionProperties, new_conn,True,False,False)
+            pmsg = str(layer.connectionProperties) + "updated"
             arcpy.AddMessage(pmsg)
 
 # get script parameter
-p = arcpy.mp.ArcGISProject('current')
-m = p.activeMap
-l = m.listLayers()
+p = arcpy.mp.ArcGISProject('current') # set project to current project
+m = p.activeMap # get current map
+layers = m.listLayers() # get layers in map
 target = arcpy.GetParameterAsText(0)
 mapping = arcpy.GetParameterAsText(1)
 
-updateconnprop(target_path=target, mapping_path=mapping)
+updateconnpropgdb(target_path=target, mapping_path=mapping, l=layers)
 
 
