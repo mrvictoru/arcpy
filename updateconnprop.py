92acd4d22c58ab20a430f2b49146a38094d5c9b9
Victor <33645782+mrvictoru@users.noreply.github.com>
2021-09-10 11:44:26 +0800

    added read json map file loop

--- updateconnprop.py
+++ updateconnprop.py
@@ -1,10 +1,20 @@
-import arcpy
+import arcpy, json, os
 
 # get script parameter
 p = arcpy.mp.ArcGISProject('current')
 m = p.activeMap
 l = m.listLayers()
 target_path = arcpy.GetParameterAsText(0)
+mapping_path = arcpy.GetParameterAsText(1)
+
+# get dataset mapping json list
+jsonlist = []
+jlist = os.listdir(mapping_path)
+for jfile in jlist:
+    jpath = os.path.join(jlist,jfile)
+    with open(jpath) as f:
+        data = json.load(f)
+        jsonlist.append(data)
 
 # indicates current map name
 pmsg = "Current map:" + m.name + "\n"
@@ -12,12 +22,11 @@ arcpy.AddMessage(pmsg)
 
 # loop through the layers in the map
 for layer in l:
-    
     try:
         arcpy.AddMessage(layer.name)
-        pmsg = str(layer.connectionProperties) + "updating"
         new_conn = layer.connectionProperties
         new_conn['connection_info'] = target_path
+        pmsg = str(layer.connectionProperties) + "updating"
         arcpy.AddMessage(pmsg)
         layer.updateConnectionProperties(layer.connectionProperties, new_conn)
         pmsg = str(layer.connectionProperties + "updated")
