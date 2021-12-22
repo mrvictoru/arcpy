1a99d9775d65d41fd54b12325dd4b8e67b4ae64c
Victor <33645782+mrvictoru@users.noreply.github.com>
2021-12-22 17:17:19 +0800

    added minor fix to path and loop through inputpath

--- transattributerule.py
+++ transattributerule.py
@@ -1,26 +1,30 @@
-import arcpy
+﻿import arcpy
 import os
 
 #DB paths and setup
 exportFromDB = arcpy.GetParameterAsText(0)
 toDB = arcpy.GetParameterAsText(1)
 saveFilesToPath = arcpy.GetParameterAsText(2)
+
+#Export attribute rules from the database
 arcpy.env.workspace = exportFromDB
 featureclasses = arcpy.ListFeatureClasses()
 
-#Export attribute rules from the database
 for fc in featureclasses:
     if(arcpy.Describe(fc).attributeRules):
-        print("Exporting attribute rules for " + fc)
-        outputPath = saveFilesToPath + fc + "_AttributeRules.csv"
+        pmsg = "Exporting attribute rules for " + str(fc)
+        arcpy.AddMessage(pmsg)
+        outputPath = os.path.join(saveFilesToPath, fc + "_AttributeRules.csv")
         arcpy.ExportAttributeRules_management(fc, outputPath)
 
-arcpy.env.workspace = toDB
-featureclasses = arcpy.ListFeatureClasses()
-
 #Import attribute rules to new database
-for fc in featureclasses:
-    if(arcpy.Describe(fc).attributeRules):
-        print("Importing attribute rules for " + fc)
-        inputPath = saveFilesToPath + fc + "_AttributeRules.csv"
+arcpy.env.workspace = toDB
+attributerules = os.listdir(saveFilesToPath)
+
+for file in attributerules:
+    if(file.endswith(".csv")):
+        fc = file.split("_")[0]
+        pmsg = "Importing attribute rules for " + str(fc)
+        arcpy.AddMessage(pmsg)
+        inputPath = os.path.join(saveFilesToPath, file)
         arcpy.ImportAttributeRules_management(fc, inputPath)
