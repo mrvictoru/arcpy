b79e3b0dc9da4cab3c78365dcba1fd909142b76b
Victor <33645782+mrvictoru@users.noreply.github.com>
2021-12-22 17:22:43 +0800

    added comments

--- transattributerule.py
+++ transattributerule.py
@@ -2,15 +2,17 @@
 import os
 
 #DB paths and setup
-exportFromDB = arcpy.GetParameterAsText(0)
-toDB = arcpy.GetParameterAsText(1)
-saveFilesToPath = arcpy.GetParameterAsText(2)
+exportFromDB = arcpy.GetParameterAsText(0) #Get the path to the database to export attribute rule from
+toDB = arcpy.GetParameterAsText(1) #Get the path to the database to import attribute rule to
+saveFilesToPath = arcpy.GetParameterAsText(2) #Get the path to save the files to
 
 #Export attribute rules from the database
 arcpy.env.workspace = exportFromDB
 featureclasses = arcpy.ListFeatureClasses()
 
+#loop through each feature class
 for fc in featureclasses:
+    #check if the feature class has attribute rules
     if(arcpy.Describe(fc).attributeRules):
         pmsg = "Exporting attribute rules for " + str(fc)
         arcpy.AddMessage(pmsg)
@@ -21,9 +23,11 @@ for fc in featureclasses:
 arcpy.env.workspace = toDB
 attributerules = os.listdir(saveFilesToPath)
 
+#loop through each attribute rule
 for file in attributerules:
+    #check if the file end with csv
     if(file.endswith(".csv")):
-        fc = file.split("_")[0]
+        fc = file.split("_")[0] #get the feature class name from the file name
         pmsg = "Importing attribute rules for " + str(fc)
         arcpy.AddMessage(pmsg)
         inputPath = os.path.join(saveFilesToPath, file)
