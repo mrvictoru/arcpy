1f640f3490e8facd6fe9076282453ef65646b00e
Victor <33645782+mrvictoru@users.noreply.github.com>
2022-01-17 10:42:53 +0800

    added comment

--- getfeaturelayernode.py
+++ getfeaturelayernode.py
@@ -26,24 +26,39 @@ def writecsv(writer,node):
     writer.writerow([node.nodename, node.recordcount, node.defquery, '\\'.join(node.nodebranches)])
 
 def main():
+    # get project file path as input parameter
     project = arcpy.GetParameterAsText(0)
+    # get output csv file path as input parameter
     output = arcpy.GetParameterAsText(1)
+    # get feature layers from project
     layers = arcpy.mp.ArcGISProject(project).listMaps()[0].listLayers()
+    # write csv header
     with open(output, 'w', newline='') as csvfile:
         writeheader(csv.writer(csvfile))
     
+    # loop through feature layers
     for layer in layers:
+        # skip annotation class 1 feature layer
         if layer.name == 'Class 1':
             continue
+        # print out layer name on console
         arcpy.AddMessage(layer.longName)
+        # check if layer is feature layer
         if layer.isFeatureLayer:
+            # create feature node object
             node = featurenode()
+            # get feature node from layer
             node.getnode(layer)
+            # write feature node to csv file
             with open(output, 'a', newline='') as csvfile:
                 writecsv(csv.writer(csvfile),node)
+        # check if layer is group layer without sublayers
         elif not(len(layer.listLayers()) > 0):
+                # create feature node object
                 node = featurenode()
+                # get feature node from layer
                 node.getemptynode(layer)
+                # write feature node to csv file
                 with open(output, 'a', newline='') as csvfile:
                     writecsv(csv.writer(csvfile),node)
 
